1f4c069be080ba534c5c35db0a29573a8cad0262
Your Name <you@example.com>
2020-02-26 16:04:12 -0600

    countWords done in objects.js

--- objects.js
+++ objects.js
@@ -4,8 +4,20 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  // const letters = Array.from(phrase);
+  let wordList = {};
+  const words = phrase.split(' ');
+  
+  for(const word of words){
+    if(word in wordList){
+      wordList[word] += 1;
+    }
+    else{
+      wordList[word] = 1;
+    }    
+  }
+  return wordList;
 }
-
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
