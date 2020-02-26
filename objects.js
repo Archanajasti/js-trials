f068b17805333c4b05bb53905261b02e540c778b
Your Name <you@example.com>
2020-02-26 16:37:32 -0600

    objects.js complete

--- objects.js
+++ objects.js
@@ -22,4 +22,15 @@ function countWords(phrase) {
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+  let melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+  if(melonPrices[price] === null){
+    return;
+  }
+  return melonPrices[price].sort();
+
 }
