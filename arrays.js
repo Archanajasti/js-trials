47402c9c6159cfce5dda788aee6f4405c83bf873
Your Name <you@example.com>
2020-02-26 12:03:06 -0600

    arrays.js in progress

--- arrays.js
+++ arrays.js
@@ -4,12 +4,34 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+ 
+  for(let i=0; i < items.length; i++){
+    console.log(items[i], i);
+  }
+  /*for (const i in items){
+      for(const item of items){
+        console.log(item, i);
+      }
+    }
+*/
 }
+
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  
+  let result = [];
+  let resultIndice = 0;
+  for(let i=0; i < items.length; i++){
+    if (i % 2 === 0){
+      result[resultIndice] = items[i];
+      resultIndice += 1;
+
+    }
+  }
+  console.log(result)
 }
 
 
